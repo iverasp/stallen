bff02583e58ff2117f4425b8ca8bf02f58529f14
Iver Egge <iver.egge@knowit.no>
2020-07-12 19:56:43 +0200

    Refactor

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
-import Bio from '../components/Bio'
-import Layout from '../components/Layout'
+import Bio from '../components/bio'
+import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
 
